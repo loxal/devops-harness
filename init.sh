c8a18be5ec4929dac9a73fbb182a7494a5caca75
Alex <alexander.orlov@loxal.net>
2017-04-29 22:16:31 +0200

    bring structure into init script

--- init.sh
+++ init.sh
@@ -1,30 +1,40 @@
 #!/usr/bin/env sh
 
-git clone git@github.com:loxal/devops-harness.git 
-
 MINION_USER=minion
 MINION_HOME=~/minion/
 
-sudo vim /etc/hosts
-sudo vim /etc/hostname
+set_hostname() {
+    sudo vim /etc/hosts
+    sudo vim /etc/hostname
+}
 
-sudo apt update
-sudo apt upgrade
+set_hostname
 
-sudo adduser --disabled-password --gecos $MINION_USER sudo
+update_system() {
+    sudo apt update
+    sudo apt upgrade
+}
+
+update_system
+
+install_cuda_driver() {
+    curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+    sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+    sudo apt update
+    sudo apt install cuda
+}
+
+install_cuda_driver
+
+sudo adduser --gecos "" $MINION_USER sudo
 sudo visudo # %sudo   ALL=(ALL:ALL) NOPASSWD:ALL
 su $MINION_USER
-
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-sudo apt update
-sudo apt install cuda
-
 # install  docker
 # enable docker
 mkdir ~/.ssh
 chmod 700 ~/.ssh
 ssh-keygen -t rsa
+cp /root/.ssh/authorized_keys ~/.ssh/
 
 
 sudo apt install tor
@@ -32,6 +42,6 @@ sudo vim /etc/tor/torrc
 sudo ln -sf $MINION_HOME/tor-exit-notice.html /etc/tor/tor-exit-notice.html
 sudo apt install docker.io
 #sudo gpasswd -a $MINION_USER docker ## delete
-sudo usermod -aG docker MINION_USER 
+sudo usermod -aG docker $MINION_USER
 # install kernel 4.10
-sudo reboot
+#sudo reboot
