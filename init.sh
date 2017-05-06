f4a996f50c8d2d8bbf9864589966ab70e4b909c9
Alex <alexander.orlov@loxal.net>
2017-05-07 00:22:06 +0200

    add sbt setup script

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 CANONICAL_LIVEPATCH_TOKEN=INSERT_YOUR_TOKEN_HERE
 MINION_USER=minion
+MINION_HOME=~/${MINION_USER}/
 
 set_hostname() {
     sudo vim /etc/hosts
@@ -42,20 +43,21 @@ update_system
 setup_minion_tool() {
     MINION_HOME=~/${MINION_USER}/
     git clone https://github.com/loxal/minion.git
-    cd $MINION_HOME
+
+    cd $MINION_HOME/miner
+    git submodule init
+    git submodule update
 }
 
 install_cuda_driver() {
-    mkdir $MINION_HOME/tmp
-    cd $MINION_HOME/tmp
-
     curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-    sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+    sudo dpkg -i cuda-repo-*.deb
+    rm cuda-repo-*.deb
     sudo apt update
 #    sudo apt install cuda
 #    sudo apt install cuda-core-8-0
 #    sudo apt install cuda-toolkit-8-0
-    sudo apt install cuda-drivers
+    sudo apt install cuda-drivers # sufficient for GPU mining
 }
 
 install_cuda_driver
@@ -78,7 +80,7 @@ setup_kernel
 
 setup_tor() {
     sudo apt install tor
-    sudo ln -s $MINION_HOME/ubuntu/etc/tor/tor-exit-notice.html /etc/tor/tor-exit-notice.html
+    sudo cp $MINION_HOME/ubuntu/etc/tor/tor-exit-notice.html /etc/tor/tor-exit-notice.html
     sudo vim /etc/tor/torrc
 }
 
---
+++ setup-etc.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+setup_scala_sbt() {
+    sudo apt install openjdk-8-jdk-headless
+    echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823
+    sudo apt update
+    sudo apt install sbt
+}
+
+setup_scala_sbt
